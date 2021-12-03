a881cabcb63f1f6295fb9b4e491608b053c5cb44
narcis <narcissprecic@gmail.com>
2021-12-03 11:09:32 -0500

    Running total update, error for budget, and circle update

--- budgetbuddy.js
+++ budgetbuddy.js
@@ -1,5 +1,10 @@
 const budget = document.getElementById("budget");
 
+const bills = document.getElementById("bills");
+const food = document.getElementById("food");
+const clothes = document.getElementById("clothes");
+const entertainment = document.getElementById("entertainment");
+
 const submitForm = document.getElementById("budget-submit-form");
 
 
@@ -9,36 +14,41 @@ submitForm.addEventListener("submit", (event) => {
     event.preventDefault();
     
     let totalBudget = budget.value;
+    let totalBills = bills.value;
+    let totalFood = food.value;
+    let totalClothes = clothes.value;
+    let totalEntertainment = entertainment.value;
+
+    if(totalBudget < (totalBills + totalFood + totalClothes + totalEntertainment)){
+       document.getElementById("image").src = "https://previews.123rf.com/images/arcady31/arcady311003/arcady31100300022/6597624-stop-sign.jpg";
+        
+    } else{
+
+        let data = [
+            { x: "Total Spent", value: totalBudget },
+            { x: "Bills", value: totalBills },
+            { x: "Clothing", value: totalFood },
+            { x: "Entertainment", value: totalClothes },
+            { x: "Food", value: totalEntertainment },
+          ];
+        
+          // create the chart
+          let chart = anychart.pie();
+        
+          // set the chart title
+          chart.title("Total Amount Spent");
+        
+          // add the data
+          chart.data(data);
+        
+          // display the chart in the container
+          chart.container("container");
+          chart.draw();
+
+    }
+
     //let totalBills = 
-    let data = [
-        { x: "Total Spent", value: totalBudget },
-        { x: "Bills", value: 0 },
-        { x: "Clothing", value: 0 },
-        { x: "Entertainment", value: 0 },
-        { x: "Food", value: 0 },
-      ];
-    
-      // create the chart
-      let chart = anychart.pie();
-    
-      // set the chart title
-      chart.title("Total Amount Spent");
-    
-      // add the data
-      chart.data(data);
-    
-      // display the chart in the container
-      chart.container("container");
-      chart.draw();
     
 });
 
-anychart.onDocumentReady(function () {
-  // set the data
-
-  let food = 540000;
-  let foodBudget = food;
-
-
-});
 
